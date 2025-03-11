ce41515faa8343568e78edfb657e90acc94f93da
evoaiman <aiman_saharudin@yahoo.com>
2025-03-11 11:44:43 +0800

    restructure to app/

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
     configPath: 'tailwind.config',
@@ -10,5 +11,8 @@ export default defineNuxtConfig({
     injectPosition: 'first',
     viewer: true,
   },
+
   plugins: ['~/plugins/preline.client.ts'],
+  srcDir: 'app/',
+  compatibilityDate: '2025-03-11',
 });
